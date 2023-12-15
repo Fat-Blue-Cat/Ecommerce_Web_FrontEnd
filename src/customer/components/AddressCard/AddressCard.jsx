7b524d3618f04cf435f0cc58f1e73eb2fcfafbeb
a39006 <vuvantrung002@gmail.com>
2023-12-15 23:07:00 +0700

    add Address Card for Order Page

---
+++ src/customer/components/AddressCard/AddressCard.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const AddressCard = () => {
+  return (
+    <div>
+      <div className="space-y-3">
+        <p className="font-semibold">Diablo Davici</p>
+        <p>Viet Nam, HaNoi, Thuong Tin 12</p>
+        <div className="space-y-1">
+          <p className="font-semibold">Phone Number</p>
+          <p>1212-121-1221</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AddressCard;
