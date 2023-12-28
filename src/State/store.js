594189034b3bde1544fe8770c614a2876ee39fdd
a39006 <vuvantrung002@gmail.com>
2023-12-28 21:58:33 +0700

    authentication for Login, Logout

---
+++ src/State/store.js
@@ -0,0 +1,10 @@
+import { authReducer } from "./Auth/Reducer";
+
+import { legacy_createStore, applyMiddleware, combineReducers } from "redux";
+import { thunk } from "redux-thunk";
+
+const rootReducers = combineReducers({
+  auth: authReducer,
+});
+
+export const store = legacy_createStore(rootReducers, applyMiddleware(thunk));
