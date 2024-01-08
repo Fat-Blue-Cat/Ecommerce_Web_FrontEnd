3ea482b5d3c8194bfd573d74671da7f80370e32f
a39006 <vuvantrung002@gmail.com>
2024-01-08 17:29:13 +0700

    fix HomeCarousel

--- src/customer/components/HomeCarousel/HomeCarouselData.js
+++ src/customer/components/HomeCarousel/HomeCarouselData.js
@@ -9,14 +9,10 @@ export const homeCarouselData = [
       "https://electshop-codezeel.myshopify.com/cdn/shop/files/main-banner-2_1903x.jpg?v=1656304950;",
     path: "/store/electronic",
   },
+
   {
     image:
-      "https://electshop-codezeel.myshopify.com/cdn/shop/files/main-banner-1_1903x.jpg?v=1656304950;",
-    path: "/store/electronic",
-  },
-  {
-    image:
-      "https://electshop-codezeel.myshopify.com/cdn/shop/files/main-banner-2_1903x.jpg?v=1656304950;",
+      "https://i.dell.com/is/image/DellContent//content/dam/ss2/product-images/page/search-creative-images/banners/2800x839/best-small-business-laptop-dell-scp.jpg?fmt=jpg&wid=2800&hei=839",
     path: "/store/electronic",
   },
   {
